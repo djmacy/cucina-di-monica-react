0fd0d8f3ebf107a46c300a43483205dac61f899a
David Macy <dmacy@carroll.edu>
2024-02-07 18:34:08 -0700

    slider must have 4 photos at least to work as intended. Need to fix footer next

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,22 +2,80 @@ import React from 'react'
 import {useState} from "react";
 import Slider from 'react-slick';
 import "./Slider.css"
+
+//icon import
+import {BsArrowLeft, BsArrowRight} from 'react-icons/bs'
+
+//images import
 import img1 from '../images/MonicaTeaching.jpg'
 import img2  from '../images/MonicaFavicon.png'
 import img3 from '../images/MonicaSmiling.jpg'
+import img4 from "../images/cropped-MonicaFavicon-3.png"
 
-const images = [img1, img2, img3];
+//Need 4 images, I dont know how to do this for 3? we'll see
+const images = [img1, img2, img3, img4];
 
+function SampleNextArrow({onClick}) {
+    return (
+        <div className = 'arrow arrow-right' onClick={onClick}>
+            <BsArrowRight/>
+        </div>
+    );
+}
+
+function SamplePrevArrow({onClick}) {
+
+    return (
+        <div className = 'arrow arrow-left' onClick={onClick}>
+            <BsArrowLeft/>
+        </div>
+    );
+}
+
+function EmptyArrow({onClick}) {
+    return (
+        <div></div>
+    );
+}
+
+//Wont let me use the name slider because its imported that way
 const Carousel = () => {
+
+    const [slideIndex, setSlideIndex] = useState(0);
+
 
     const  settings = {
         dots: true,
         infinite: true,
-        speed: 500,
+        speed: 1000,
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 1000
+        autoplaySpeed: 2000,
+        beforeChange: (current, next) => setSlideIndex(next),
+        centerMode: true,
+        nextArrow: <SampleNextArrow/>,
+        prevArrow: <SamplePrevArrow/>,
+        appendDots: (dots) => (
+            <div>
+                <ul style={{ margin: "0px" }}> {dots} </ul>
+            </div>
+        ),
+        customPaging: (current, next) => (
+            <div className={current === slideIndex ? 'dot dot-active' : 'dot'}>
+            </div>
+        ),
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false,
+                    nextArrow: <EmptyArrow />,
+                    prevArrow: <EmptyArrow />,
+                }
+            }]
     };
 
     return (
@@ -26,8 +84,8 @@ const Carousel = () => {
                 <div className="slider">
                     <Slider {...settings}>
                         {
-                            images.map((img)=> (
-                                <div className="slide" key={img}>
+                            images.map((img, index)=> (
+                                <div className={index === slideIndex ? 'slide slide-active' : "slide"} key={index}>
                                     <img src={img} alt="" />
                                 </div>
                             ))
@@ -38,4 +96,4 @@ const Carousel = () => {
     );
 };
 
-export default Slider;
+export default Carousel;
