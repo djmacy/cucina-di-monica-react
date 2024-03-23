e17bf5ca67537a771b5e2e2b313c5d124614cdae
David Macy <dmacy@carroll.edu>
2024-03-23 12:21:21 -0600

    added two logos for testing purposes

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,7 +1,7 @@
 // Logo.js
 import React from 'react';
 import './Logo.css'
-import logoImage from '../images/cropped-MonicaFavicon-3.png'; // Replace 'logo.png' with the path to your logo image
+import logoImage from '../images/remodeledForkSpoon.png'; // Replace 'logo.png' with the path to your logo image
 
 const Logo = () => {
     return (
